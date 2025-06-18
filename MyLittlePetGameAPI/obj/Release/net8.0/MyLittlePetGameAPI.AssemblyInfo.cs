807b524202ff1cc17e849bb1ed433edb88bba30a
MinhTran19o11 <minhtkdse182196@fpt.edu.vn>
2025-06-18 09:06:52 +0700

    Modify StartScreen

--- MyLittlePetGameAPI/obj/Release/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Release/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b250801dd41eb7b19b13b96e090566fc5402c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de02ec57ab756ebddf50e0fa696ffab923fbd1f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
