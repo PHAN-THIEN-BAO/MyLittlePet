05a7b2860297c8aec72b9a616b209cb76d0c91e7
PHAN-THIEN-BAO <phanthienbao0612@gmail.com>
2025-06-24 16:54:59 +0700

    merge

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481486da2f25be8bd3eaeba04a8e9eb6b7c02ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92321f68c3b7a49b7fd28415e550bd0b170bfd21")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
