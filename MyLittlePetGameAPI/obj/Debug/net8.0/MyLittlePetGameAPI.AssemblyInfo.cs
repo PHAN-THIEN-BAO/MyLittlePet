04685b3d3bf4aa28a167e0822e89a71d8365eec3
htqnhat280203 <nhathtqde170475@fpt.edu.vn>
2025-06-24 15:49:18 +0700

    update Aquarium scence minigame

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7bd9d642a0ad7dd79b2d0144b8b1ba9eadfccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf91d52cab51099c6b59b28e20da808eddafa7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
