3af3a2aa2645e26a9310c1299e3e43b871343de4
PHAN-THIEN-BAO <phanthienbao0612@gmail.com>
2025-06-27 05:58:02 +0700

    fix bug

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da15e8a4b0626b39811b29609ed8441b449e335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3a8963efc66acc968750ea9c642facc8d01477")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
