39213119f6146d7a8f73cbf4d4f0078b30d6a78d
MinhTran19o11 <minhtkdse182196@fpt.edu.vn>
2025-06-28 05:18:36 +0700

    interactions

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca93031a65a13b6f216228a4dad5e196ff43d69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5260554494419c5ab9b890536a318ffffa418dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
