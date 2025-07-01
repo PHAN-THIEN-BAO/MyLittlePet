4a75b3bb46a8c4751d7eb5e35de88fc650a923f4
MinhTran19o11 <minhtkdse182196@fpt.edu.vn>
2025-07-01 21:26:17 +0700

    Character Animation

---
+++ Assets/Scripts/Cutscene/DialogueLine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+        [SerializeField] private string input;
+
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+            
+            StartCoroutine(WriteText(input, textHolder));
+        }
+    }
+
+}
