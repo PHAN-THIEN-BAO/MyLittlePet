ac47551fdb1b34573af84a600ebe823bb15abaf8
htqnhat280203 <nhathtqde170475@fpt.edu.vn>
2025-07-07 11:05:31 +0700

    update sample scene

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8ee5e92d53ca82e148a7ca616cfdad2b25fd3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadb9f359b75cf8fe9cdb63efe0bd2c9836a16d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
