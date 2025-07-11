77f87a917ce9eb25832fa3e828cc40287730fbf8
htqnhat280203 <nhathtqde170475@fpt.edu.vn>
2025-07-11 14:08:07 +0700

    update landscape

--- Assets/Scripts/FarmAction/Item.cs
+++ Assets/Scripts/FarmAction/Item.cs
@@ -1,16 +1,17 @@
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Item : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//[RequireComponent(typeof(Rigidbody2D))] // Yêu cầu GameObject có Rigidbody2D
+//public class Item : MonoBehaviour
+//{
+//    public ItemData data; // Dữ liệu của item
+
+//    [HidenInspector] public Rigidbody2D rb2d; // Rigidbody2D để xử lý vật lý
+
+//    private void Awake()
+//    {
+//        rb2d = GetComponent<Rigidbody2D>(); // Lấy Rigidbody2D từ GameObject
+//    }
+//}
