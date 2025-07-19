edfc8797d67c4482d7e2985a8e2fe72f7d014f10
MinhTran19o11 <minhtkdse182196@fpt.edu.vn>
2025-07-19 07:45:24 +0700

    Intro scene modifications

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce7c9a4d8d7d75cc870fe0c7e4381fdb7f280c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531a912935ae296ccab93357ea5cf3195b12ff4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
