8422ee79c6d6c9987e09ea3f1ba5a37c17adeed2
MinhTran19o11 <minhtkdse182196@fpt.edu.vn>
2025-07-25 00:04:45 +0700

    Fixed NPC movement

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19b9240b9e322d7fe62d23d438b4ba9fccb4998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a86698e0780c315e822a6ae4357910b62737fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
