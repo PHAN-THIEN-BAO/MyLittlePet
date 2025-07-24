de98d1f762f789902df54fc3d634e8de20cda049
MinhTran19o11 <minhtkdse182196@fpt.edu.vn>
2025-07-25 00:48:53 +0700

    add Hieu Minigame

--- MyLittlePetGameAPI/My_Little_Pet_V3 .sql
+++ MyLittlePetGameAPI/My_Little_Pet_V3 .sql
@@ -11,13 +11,111 @@ END"  */
 --4 CHAY PHAN CON LAI
 
 
-Drop database My_Little_Pet_V3;
-select * from ShopProduct
-drop table PlayerPet
---GO
-select * from [User]
-Use My_Little_Pet_V3;
-Drop database My_Little_Pet_V3;
+
+--Go
+INSERT INTO [User] (Role, UserName, Email, Password, Level, Coin, Diamond, Gem)
+VALUES 
+('Player', N'CatLover01', 'catlover01@example.com', 'pass1234', 5, 1000, 5, 3),
+('Player', N'DogMaster99', 'dogmaster99@example.com', 'dogpass99', 3, 800, 2, 1),
+('Admin', N'AdminPetCare', 'admin@petgame.com', 'adminpass', 10, 5000, 50, 20),
+('Player', N'BunnyQueen', 'bunnyq@example.com', 'bunny123', 2, 300, 0, 0),
+('Player', N'HamsterHero', 'hamhero@example.com', 'hamham', 4, 700, 3, 2),
+('Admin', N'ModPuppy', 'modpuppy@example.com', 'mod123', 6, 2000, 10, 5),
+('Player', N'KittyCraze', 'kittycraze@example.com', 'meowmeow', 1, 100, 0, 0),
+('Player', N'FishyFella', 'fishyfella@example.com', 'fishfish', 3, 600, 1, 1),
+('Player', N'BirdWatcher', 'birdw@example.com', 'tweet123', 5, 1200, 6, 4),
+('Player', N'ReptileRider', 'reptrider@example.com', 'reptilepass',7, 1500, 8, 7);
+
+
+INSERT INTO Shop (Name, Type, Description)
+VALUES 
+('Pets Shop', 'Pet', 'Pet adoption and animal companions'),
+('Item Shop', 'Item', 'Supplies, food and toys for pets');
+
+INSERT INTO Pet (AdminID, PetType, PetDefaultName, Description)
+VALUES 
+(1, 'Cat', 'Brown Cat', 'A calm and cuddly brown cat who loves to nap in the sun.'),
+(1, 'Cat', 'White Cat', 'A playful white cat with bright blue eyes and a curious nature.'),
+(1, 'Cat', 'Orange Cat', 'A mischievous orange cat full of energy and charm.'),
+(1, 'Chicken', 'Chicken', 'A cheerful chicken that lays eggs and enjoys pecking around the yard.'),
+(1, 'Peacock', 'Peacock', 'A majestic peacock with shimmering feathers and an elegant strut.'),
+(1, 'Porcupine', 'Porcupine', 'A shy porcupine with spiky quills and a big heart.');
+
+
+INSERT INTO ShopProduct (ShopID, AdminID,PetID, Name, Type, Description, ImageUrl, Price, CurrencyType)
+VALUES 
+-- Pets Shop (ShopID = 1)
+(1, 1,1, 'Brown Cat', 'Pet', 'A calm and cuddly brown cat who loves to nap in the sun.', 'https://drive.google.com/file/d/1RaHsGrKbr0gMZqQzIL1DS0JfZtAGre1P/view', 100, 'Coin'),
+(1, 1,2, 'White Cat', 'Pet', 'A playful white cat with bright blue eyes and a curious nature.', 'https://drive.google.com/file/d/12b1oAHdgh4bfoPBNHVd-EaJZMmQl4b0J/view', 120, 'Coin'),
+(1, 1,3, 'Orange Cat', 'Pet', 'A mischievous orange cat full of energy and charm.', 'https://drive.google.com/file/d/1505vN47KRjXfx3gM5NbHVSnYVN2gpWdG/view', 150, 'Coin'),
+(1, 1,4, 'Chicken', 'Pet', 'A cheerful chicken that lays eggs and enjoys pecking around the yard.', 'https://drive.google.com/file/d/1fsJXvABMVtfGSPJz7E-_yhqv0H7Fo8oS/view', 300, 'Coin'),
+(1, 1,5, 'Peacock', 'Pet', 'A majestic peacock with shimmering feathers and an elegant strut.', 'https://drive.google.com/file/d/1GE8iqscHwOYsgTuO36Lx09PiEc6ct4WH/view', 15, 'Gem'),
+(1, 1,6, 'Porcupine', 'Pet', 'A shy porcupine with spiky quills and a big heart.', 'https://drive.google.com/file/d/1SfqfPU2KDpSEmVLvXqupFVs7q5PLcD0I/view', 35, 'Diamond');
+
+
+
+
+-- Item Shop (ShopID = 2)
+(2, 1,NULL, 'Cookies', 'Food', 'Sweet and crunchy cookies for your pet to enjoy', 'https://drive.google.com/file/d/1BI3P_--YCN0OQvrpeEWVK1l0dbIPFLOt/view', 40, 'Coin'),
+(2, 1,NULL, 'Chocolate', 'Food', 'A special chocolate treat (non-toxic for pets)', 'https://drive.google.com/file/d/1eeJ-Tx6ztnARuhZF3rTvThSEb1uR9ScH/view', 40, 'Coin'),
+(2, 1,NULL, 'Orange', 'Food', 'Fresh and juicy orange slices full of vitamins', 'https://drive.google.com/file/d/1Mz8_kpl7E1_MAlQ_IF1vdeXpZwEKLK0Y/view', 40, 'Coin'),
+(2, 1,NULL, 'Cherry', 'Food', 'Sweet cherries that boost pet energy', 'https://drive.google.com/file/d/1vRZIx7kORpayLeHnxZSpJ1LwIGcsgrx4/view', 40, 'Coin'),
+(2, 1,NULL, 'Pear', 'Food', 'Ripe and juicy pears for healthy digestion', 'https://drive.google.com/file/d/1Rm5aSvUASG_KzZMIWWLaSmVOx8f0Ts-8/view', 40, 'Coin'),
+(2, 1,NULL, 'Banana', 'Food', 'Soft and sweet bananas loved by all pets', 'https://drive.google.com/file/d/1gJtCKHc1IBc_JRGLd29yaYbaCUETSM8A/view', 40, 'Coin');
+
+Select * from ShopProduct;
+
+UPDATE ShopProduct
+SET Type = 'Pet'
+WHERE ShopID = 1 AND PetID IS NOT NULL;
+
+
+
+
+
+
+INSERT INTO PlayerPet (PlayerID, PetID, PetCustomName, Status)
+VALUES 
+(2, 1, 'Whiskers', '100%50%20'),
+(2, 2, 'Barker', '50%50%100'),
+(1, 1, 'Fluffy', '100%100%100'),
+(2, 1, 'Speedy', '20%50%100'),
+(4, 2, 'Talky', '100%50%20'),
+(1, 3, 'Shell', '50%50%20'),
+(2, 1, 'Splash', '20%20%50'),
+(5, 2, 'Firetail', '100%20%50'),
+(7, 3, 'Bamboo', '50%50%20'),
+(10, 2, 'Skyflame', '100%20%50');
+INSERT INTO CareActivity (ActivityType, Description)
+VALUES 
+('Feed', 'Give food to pet'),
+('Sleep', 'Put pet to rest'),
+('Play', 'Play with your pet');
+INSERT INTO CareHistory (PlayerPetID, PlayerID, ActivityID)
+VALUES 
+(1, 3, 1),
+(1, 3, 2),
+(2, 3, 3),
+(3, 4, 1),
+(5, 6, 2),
+(6, 6, 3),
+(7, 7, 1),
+(8, 7, 2);
+INSERT INTO Achievement (AchievementName, Description)
+VALUES
+('Welcome Aboard!', 'First login to receive a reward'),
+('Pet Collector I', 'Own 2 pets to receive a reward'),
+('Pet Collector II', 'Own 4 pets to receive a reward'),
+('Pet Collector III', 'Own 6 pets to receive a reward'),
+('Wealthy I', 'Own 1,000 coins to receive a reward'),
+('Wealthy II', 'Own 5,000 coins to receive a reward'),
+('Wealthy III', 'Own 10,000 coins to receive a reward');
+
+INSERT INTO Minigame (Name, Description)
+VALUES 
+('Dark', 'Escape room');
+
+
 CREATE TABLE [User] (
     ID INT PRIMARY KEY  IDENTITY(1,1),
 	Role NVARCHAR(50) NOT NULL,
@@ -31,6 +129,32 @@ CREATE TABLE [User] (
     JoinDate DATETIME DEFAULT GETDATE(),
 	Position FLOAT,
 	EXP INT
+);
+
+--Done with Shop and Player 
+CREATE TABLE Pet (
+    PetID INT PRIMARY KEY IDENTITY(1,1),
+	AdminID INT,
+    PetType VARCHAR(50) NOT NULL,  
+	PetDefaultName VARCHAR(50) NOT NULL,
+	PetStatus INT DEFAULt 1,
+    Description TEXT,
+	FOREIGN KEY (AdminID) REFERENCES [User](ID)
+);
+
+
+
+CREATE TABLE PlayerPet (
+    PlayerPetID INT PRIMARY KEY IDENTITY(1,1),
+    PlayerID INT NOT NULL,
+    PetID INT NOT NULL,
+    PetCustomName VARCHAR(50),             
+    AdoptedAt DATETIME DEFAULT GETDATE(),
+	UNIQUE(PlayerID, PetCustomName),
+	Status NVARCHAR(50),
+    LastStatusUpdate DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (PlayerID) REFERENCES [User](ID),
+    FOREIGN KEY (PetID) REFERENCES Pet(PetID)
 );
 
 
@@ -73,32 +197,7 @@ CREATE TABLE PlayerInventory (
 );
 
 
---Done with Shop and Player 
-CREATE TABLE Pet (
-    PetID INT PRIMARY KEY IDENTITY(1,1),
-	AdminID INT,
-    PetType VARCHAR(50) NOT NULL,  
-	PetDefaultName VARCHAR(50) NOT NULL,
-	PetStatus INT DEFAULt 1,
-    Description TEXT,
-	FOREIGN KEY (AdminID) REFERENCES [User](ID)
-);
 
-ALTER TABLE PlayerPet
-DROP COLUMN Description;
-
-CREATE TABLE PlayerPet (
-    PlayerPetID INT PRIMARY KEY IDENTITY(1,1),
-    PlayerID INT NOT NULL,
-    PetID INT NOT NULL,
-    PetCustomName VARCHAR(50),             
-    AdoptedAt DATETIME DEFAULT GETDATE(),
-	UNIQUE(PlayerID, PetCustomName),
-	Status NVARCHAR(50),
-    LastStatusUpdate DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (PlayerID) REFERENCES [User](ID),
-    FOREIGN KEY (PetID) REFERENCES Pet(PetID)
-);
 --C n playerPet vs ACtivity
 CREATE TABLE CareActivity (
     ActivityID INT PRIMARY KEY IDENTITY(1,1),
@@ -176,6 +275,111 @@ BEGIN
     INNER JOIN inserted i ON SP.ShopProductID = i.ShopProductID
     WHERE i.ImageUrl LIKE '%drive.google.com/file/d/%';
 END
+
+
+
+INSERT INTO [User] (Role, UserName, Email, Password, Level, Coin, Diamond, Gem)
+VALUES 
+('Player', N'CatLover01', 'catlover01@example.com', 'pass1234', 5, 1000, 5, 3),
+('Player', N'DogMaster99', 'dogmaster99@example.com', 'dogpass99', 3, 800, 2, 1),
+('Admin', N'AdminPetCare', 'admin@petgame.com', 'adminpass', 10, 5000, 50, 20),
+('Player', N'BunnyQueen', 'bunnyq@example.com', 'bunny123', 2, 300, 0, 0),
+('Player', N'HamsterHero', 'hamhero@example.com', 'hamham', 4, 700, 3, 2),
+('Admin', N'ModPuppy', 'modpuppy@example.com', 'mod123', 6, 2000, 10, 5),
+('Player', N'KittyCraze', 'kittycraze@example.com', 'meowmeow', 1, 100, 0, 0),
+('Player', N'FishyFella', 'fishyfella@example.com', 'fishfish', 3, 600, 1, 1),
+('Player', N'BirdWatcher', 'birdw@example.com', 'tweet123', 5, 1200, 6, 4),
+('Player', N'ReptileRider', 'reptrider@example.com', 'reptilepass',7, 1500, 8, 7);
+
+
+INSERT INTO Shop (Name, Type, Description)
+VALUES 
+('Pets Shop', 'Pet', 'Pet adoption and animal companions'),
+('Item Shop', 'Item', 'Supplies, food and toys for pets');
+
+INSERT INTO Pet (AdminID, PetType, PetDefaultName, Description)
+VALUES 
+(1, 'Cat', 'Brown Cat', 'A calm and cuddly brown cat who loves to nap in the sun.'),
+(1, 'Cat', 'White Cat', 'A playful white cat with bright blue eyes and a curious nature.'),
+(1, 'Cat', 'Orange Cat', 'A mischievous orange cat full of energy and charm.'),
+(1, 'Chicken', 'Chicken', 'A cheerful chicken that lays eggs and enjoys pecking around the yard.'),
+(1, 'Peacock', 'Peacock', 'A majestic peacock with shimmering feathers and an elegant strut.'),
+(1, 'Porcupine', 'Porcupine', 'A shy porcupine with spiky quills and a big heart.');
+
+
+INSERT INTO ShopProduct (ShopID, AdminID,PetID, Name, Type, Description, ImageUrl, Price, CurrencyType)
+VALUES 
+-- Pets Shop (ShopID = 1)
+(1, 1,1, 'Brown Cat', 'Pet', 'A calm and cuddly brown cat who loves to nap in the sun.', 'https://drive.google.com/file/d/1RaHsGrKbr0gMZqQzIL1DS0JfZtAGre1P/view', 100, 'Coin'),
+(1, 1,2, 'White Cat', 'Pet', 'A playful white cat with bright blue eyes and a curious nature.', 'https://drive.google.com/file/d/12b1oAHdgh4bfoPBNHVd-EaJZMmQl4b0J/view', 120, 'Coin'),
+(1, 1,3, 'Orange Cat', 'Pet', 'A mischievous orange cat full of energy and charm.', 'https://drive.google.com/file/d/1505vN47KRjXfx3gM5NbHVSnYVN2gpWdG/view', 150, 'Coin'),
+(1, 1,4, 'Chicken', 'Pet', 'A cheerful chicken that lays eggs and enjoys pecking around the yard.', 'https://drive.google.com/file/d/1fsJXvABMVtfGSPJz7E-_yhqv0H7Fo8oS/view', 150, 'Coin'),
+(1, 1,5, 'Peacock', 'Pet', 'A majestic peacock with shimmering feathers and an elegant strut.', 'https://drive.google.com/file/d/1GE8iqscHwOYsgTuO36Lx09PiEc6ct4WH/view', 150, 'Coin'),
+(1, 1,6, 'Porcupine', 'Pet', 'A shy porcupine with spiky quills and a big heart.', 'https://drive.google.com/file/d/1SfqfPU2KDpSEmVLvXqupFVs7q5PLcD0I/view', 150, 'Coin'),
+
+
+
+
+-- Item Shop (ShopID = 2)
+(2, 1,NULL, 'Cookies', 'Food', 'Sweet and crunchy cookies for your pet to enjoy', 'https://drive.google.com/file/d/1BI3P_--YCN0OQvrpeEWVK1l0dbIPFLOt/view', 40, 'Coin'),
+(2, 1,NULL, 'Chocolate', 'Food', 'A special chocolate treat (non-toxic for pets)', 'https://drive.google.com/file/d/1eeJ-Tx6ztnARuhZF3rTvThSEb1uR9ScH/view', 40, 'Coin'),
+(2, 1,NULL, 'Orange', 'Food', 'Fresh and juicy orange slices full of vitamins', 'https://drive.google.com/file/d/1Mz8_kpl7E1_MAlQ_IF1vdeXpZwEKLK0Y/view', 40, 'Coin'),
+(2, 1,NULL, 'Cherry', 'Food', 'Sweet cherries that boost pet energy', 'https://drive.google.com/file/d/1vRZIx7kORpayLeHnxZSpJ1LwIGcsgrx4/view', 40, 'Coin'),
+(2, 1,NULL, 'Pear', 'Food', 'Ripe and juicy pears for healthy digestion', 'https://drive.google.com/file/d/1Rm5aSvUASG_KzZMIWWLaSmVOx8f0Ts-8/view', 40, 'Coin'),
+(2, 1,NULL, 'Banana', 'Food', 'Soft and sweet bananas loved by all pets', 'https://drive.google.com/file/d/1gJtCKHc1IBc_JRGLd29yaYbaCUETSM8A/view', 40, 'Coin');
+
+Select * from ShopProduct;
+
+UPDATE ShopProduct
+SET Type = 'Pet'
+WHERE ShopID = 1 AND PetID IS NOT NULL;
+
+
+
+
+
+
+INSERT INTO PlayerPet (PlayerID, PetID, PetCustomName, Status)
+VALUES 
+(2, 1, 'Whiskers', '100%50%20'),
+(2, 2, 'Barker', '50%50%100'),
+(1, 1, 'Fluffy', '100%100%100'),
+(2, 1, 'Speedy', '20%50%100'),
+(4, 2, 'Talky', '100%50%20'),
+(1, 3, 'Shell', '50%50%20'),
+(2, 1, 'Splash', '20%20%50'),
+(5, 2, 'Firetail', '100%20%50'),
+(7, 3, 'Bamboo', '50%50%20'),
+(10, 2, 'Skyflame', '100%20%50');
+INSERT INTO CareActivity (ActivityType, Description)
+VALUES 
+('Feed', 'Give food to pet'),
+('Sleep', 'Put pet to rest'),
+('Play', 'Play with your pet');
+INSERT INTO CareHistory (PlayerPetID, PlayerID, ActivityID)
+VALUES 
+(1, 3, 1),
+(1, 3, 2),
+(2, 3, 3),
+(3, 4, 1),
+(5, 6, 2),
+(6, 6, 3),
+(7, 7, 1),
+(8, 7, 2);
+INSERT INTO Achievement (AchievementName, Description)
+VALUES
+('Welcome Aboard!', 'First login to receive a reward'),
+('Pet Collector I', 'Own 2 pets to receive a reward'),
+('Pet Collector II', 'Own 4 pets to receive a reward'),
+('Pet Collector III', 'Own 6 pets to receive a reward'),
+('Wealthy I', 'Own 1,000 coins to receive a reward'),
+('Wealthy II', 'Own 5,000 coins to receive a reward'),
+('Wealthy III', 'Own 10,000 coins to receive a reward');
+
+INSERT INTO Minigame (Name, Description)
+VALUES 
+('Dark', 'Escape room');
+
 
 
 
--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5212ba4c770e18ff1db4d82989133112720c1880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9666d51f1b898bd1d31b0a6c512d4c4008c32b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
