e01325d4e46cb55d59d02eb8687896c422479fea
Lê Ngọc Hậu <147742052+ngochau369@users.noreply.github.com>
2025-07-29 18:01:58 +0700

    Fix

--- MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
+++ MyLittlePetGameAPI/obj/Debug/net8.0/MyLittlePetGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda95a4024f2d802e02064462adef1f9d936cc61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19b9240b9e322d7fe62d23d438b4ba9fccb4998")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLittlePetGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
